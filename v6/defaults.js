9f28c13183e9ae256509b86ddcfad895e66869b8
shawkattack <scherzinger.david@gmail.com>
2012-12-08 17:39:10 -0600

    Reorganize config... again

---
+++ v6/defaults.js
@@ -0,0 +1,8 @@
+module.exports.defaults = {
+    utils: './proto/utils.js',
+    queue: './proto/queue.js',
+    aways: './proto/aways.js',
+    kicks: './proto/kicks.js',
+    limit: './proto/limit.js',
+    help:  './proto/help.js'
+}
